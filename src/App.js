f967f5712a2980aee9fe36c9ba2cf252a7bf0d24
Eugene Leto <mholy@protonmail.ch>
2020-10-03 16:42:29 +0500

    5.5-5.10

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import LoginForm from 'components/LoginForm'
 import BlogForm from 'components/BlogForm'
+import Togglable from 'components/Togglable'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -14,6 +15,8 @@ const App = () => {
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const [successMessage, setSuccessMessage] = useState(null)
+  const [sorted, setSorted] = useState(null)
+  const blogFormRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
@@ -55,6 +58,7 @@ const App = () => {
   const createBlog = async (blogObject) => {
     try {
       const newBlog = await blogService.create(blogObject)
+      blogFormRef.current.toggleVisibility()
       setBlogs(blogs.concat(newBlog))
       setSuccessMessage(
         `a new blog ${blogObject.title} by ${blogObject.author} added`
@@ -64,6 +68,50 @@ const App = () => {
       setErrorMessage(error.message)
       setTimeout(() => setErrorMessage(null), 5000)
     }
+  }
+
+  const likeBlog = async (id) => {
+    const blog = blogs.find((b) => b.id === id)
+    const changedBlog = { ...blog, likes: blog.likes + 1 }
+
+    try {
+      const response = await blogService.mutate(id, changedBlog)
+      setBlogs(blogs.map((b) => (b.id !== id ? b : response)))
+
+      setSuccessMessage(`You like ${blog.title} by ${blog.author}`)
+      setTimeout(() => setSuccessMessage(null), 5000)
+    } catch (error) {
+      setErrorMessage(error.message)
+      setTimeout(() => setErrorMessage(null), 5000)
+    }
+  }
+
+  const removeBlog = async (id) => {
+    const blog = blogs.find((b) => b.id === id)
+
+    try {
+      await blogService.deleteById(id)
+      setBlogs(blogs.filter((b) => b.id !== id))
+
+      setSuccessMessage(`Removed ${blog.title} by ${blog.author}`)
+      setTimeout(() => setSuccessMessage(null), 5000)
+    } catch (error) {
+      setErrorMessage(error.message)
+      setTimeout(() => setErrorMessage(null), 5000)
+    }
+  }
+
+  const handleSort = () => {
+    let newSort = sorted === 'desk' ? 'ask' : 'desk'
+
+    setBlogs(
+      blogs.sort((a, b) => {
+        if (!sorted || newSort === 'desk') return b.likes - a.likes
+        if (newSort === 'ask') return a.likes - b.likes
+        return null
+      })
+    )
+    setSorted(newSort)
   }
 
   if (user === null)
@@ -91,9 +139,20 @@ const App = () => {
       </p>
       <Notification message={errorMessage} className="error" />
       <Notification message={successMessage} className="success" />
-      <BlogForm createBlog={createBlog} />
+      <Togglable buttonLabel="create blog" ref={blogFormRef}>
+        <BlogForm createBlog={createBlog} />
+      </Togglable>
+      <button onClick={handleSort} type="button">
+        sort by likes
+      </button>
       {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
+        <Blog
+          key={blog.id}
+          blog={blog}
+          likeBlog={likeBlog}
+          removeBlog={removeBlog}
+          createdByUser={user.name === blog.user.name}
+        />
       ))}
     </div>
   )
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,56 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog, likeBlog, removeBlog, createdByUser }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const handleBlogLike = () => {
+    likeBlog(blog.id)
+  }
+
+  const handleRemove = () => {
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`))
+      removeBlog(blog.id)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const showWhenVisible = { display: showDetails ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={toggleDetails} type="button">
+          {showDetails ? 'hide' : 'view'}
+        </button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes}
+          <button onClick={handleBlogLike} type="button">
+            like
+          </button>
+        </div>
+        <div>{blog.author}</div>
+        {createdByUser && (
+          <button onClick={handleRemove} type="button">
+            Remove
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
